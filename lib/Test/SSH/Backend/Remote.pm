f9e7a70481d1c5e4b9c5ab41afd760c0103dd940
Salvador <sfandino@yahoo.com>
2013-01-23 18:49:16 +0100

    working on it

---
+++ lib/Test/SSH/Backend/Remote.pm
@@ -0,0 +1,33 @@
+package Test::SSH::Backend::Remote;
+
+use strict;
+use warnings;
+
+require Test::SSH::Backend::Base;
+our @ISA = qw(Test::SSH::Backend::Base);
+
+sub new {
+    my ($class, %opts) = @_;
+    my $sshd = $class->SUPER::new(%opts);
+
+    if (defined $sshd->{password}) {
+        $sshd->{auth_method} = 'password';
+        if ($sshd->_test_server) {
+            $sshd->_log("the given password can be used to connect to host");
+            return $sshd;
+        }
+    }
+
+    $sshd->{auth_method} = 'publickey';
+    for my $key (@{$sshd->{user_keys}}) {
+        $sshd->_log("trying user key '$key'");
+        $sshd->{private_key_path} = $key;
+        if ($sshd->_test_server) {
+            $sshd->_log("key '$key' can be used to connect to host");
+            return $sshd;
+        }
+    }
+    ()
+}
+
+1;
